c15be513cfeae5608962869fca2323900b516263
Julian <julian.bildner@leanio.de>
2023-03-06 11:38:29 +0100

    Apply suggestions from code review

--- KundenWebSystem/Services/BookingService.cs
+++ KundenWebSystem/Services/BookingService.cs
@@ -17,7 +17,11 @@ namespace KundenWebSystem.Data
 
         public async Task<List<tbl_Buchungen>> GetBuchungenForUserAsync(int userId)
         {
-            return await this.databaseContext.tbl_Buchungen.Include(o => o.ed_EvDaten).Include(o => o.ed_EvDaten.et_Event).Where(buchung => buchung.kd_KundenID == userId).ToListAsync();
+            return await this.databaseContext.tbl_Buchungen
+                .Include(o => o.ed_EvDaten)
+                .Include(o => o.ed_EvDaten.et_Event)
+                .Where(buchung => buchung.kd_KundenID == userId)
+                .ToListAsync();
         }
 
         public async Task<tbl_EventDaten> GetEventDatenFromId(int eventDatenID)
@@ -25,14 +29,14 @@ namespace KundenWebSystem.Data
             return await this.databaseContext.tbl_EventDaten
                 .Include(o => o.et_Event.ev_EvVeranstalter)
                 .Include(o => o.et_Event.ek_EvKategorie)
-                .Where(eventDaten => eventDaten.ed_EvDatenID == eventDatenID)
-                .FirstOrDefaultAsync();
+                .FirstOrDefaultAsync(eventDaten => eventDaten.ed_EvDatenID == eventDatenID);
         }
 
         public async Task<int> GetEventDatenIdFromBuchungId(int buchungId)
         {
-            tbl_Buchungen buchung = await this.databaseContext.tbl_Buchungen.Where(buchung => buchung.bu_BuchungsID == buchungId).FirstOrDefaultAsync();
-            return buchung == null ? -1 : buchung.ed_EvDatenID;
+            tbl_Buchungen buchung = await this.databaseContext.tbl_Buchungen
+                .FirstOrDefaultAsync(buchung => buchung.bu_BuchungsID == buchungId);
+            return buchung?.ed_EvDatenID ?? -1;
         }
     }
 
