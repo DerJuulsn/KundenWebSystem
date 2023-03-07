a583b810ddded00a58a4252bf53655a156710ac4
DaniOfficial <winke-daniel@t-online.de>
2023-03-07 13:48:04 +0100

    Registrierungsseite

---
+++ KundenWebSystem.Database/ModelExtension/KundenExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KundenWebSystem.Database.Model
+{
+    partial class tbl_Kunden
+    {
+        public string PasswortWiederholen { get; set; }
+    }
+}
